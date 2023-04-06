3c9c6ef180cb5c66a9a325b751dafd1d10fc4130
joelcmk <57118300+joelcmk@users.noreply.github.com>
2023-04-06 16:36:03 -0500

    Updated budget component

--- Components/Budget/Budget.tsx
+++ Components/Budget/Budget.tsx
@@ -1,61 +1,70 @@
 import React, { useState, useEffect } from 'react';
+import styles from './Budget.module.css';
 
 function Budget() {
   const [target, setTarget] = useState(true);
 
   const [updateTarget, setUpdateTarget] = useState(false);
 
-  if (target) {
-    return (
-      <>
-        <div className="budget">
-          <div className="test">
-            <table className="expenses">
-              <tr>
-                <th>Category</th>
-                <th>Expenses</th>
-                <th>Target</th>
-              </tr>
-
-              <>
-                <tr>
-                  <td>Name</td>
-                  <td>
-                    <span>$</span>
-                    222
-                  </td>
-                  <td>
-                    <div className="edit">
-                      <p className="">
-                        <span>$</span>0
-                      </p>
-                    </div>
-                    ) : (
-                    <div>
-                      <input type="number" className="input" />
-                    </div>
-                    )
-                  </td>
-                </tr>
-              </>
-
-              <tr>
-                <td></td>
-                <td></td>
-                <td>
-                  <button className="update_button">
-                    {!updateTarget ? 'update' : 'done'}
-                  </button>
-                </td>
-              </tr>
-            </table>
-          </div>
-        </div>
-      </>
-    );
-  } else {
-    return <p>loading</p>;
-  }
+  return (
+    <div className={styles.budget}>
+      <div className={styles.wrapper}>
+        <table className={styles.expenses}>
+          <tbody>
+            <tr>
+              <th>Category</th>
+              <th>Expenses</th>
+              <th>Target</th>
+            </tr>
+            <tr>
+              <td>Home</td>
+              <td>$25</td>
+              <td>$25</td>
+            </tr>
+            <tr>
+              <td>Food</td>
+              <td>$30</td>
+              <td>$25</td>
+            </tr>
+            <tr>
+              <td>Shopping</td>
+              <td>$30</td>
+              <td>$25</td>
+            </tr>
+            <tr>
+              <td>Utilities</td>
+              <td>$30</td>
+              <td>$25</td>
+            </tr>
+            <tr>
+              <td>Household</td>
+              <td>$30</td>
+              <td>$25</td>
+            </tr>
+            <tr>
+              <td>Transportation</td>
+              <td>$30</td>
+              <td>$25</td>
+            </tr>
+            <tr>
+              <td>Ohter</td>
+              <td>$30</td>
+              <td>$25</td>
+            </tr>
+            <tr>
+              <td></td>
+              <td></td>
+              <td>
+                <button className={styles.update_button}>
+                  {!updateTarget ? 'update' : 'done'}
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
 
 export default Budget;
--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,13 +1,13 @@
 import Nav from '@/Components/Nav/Nav';
 import Footer from '../../Components/Footer/Footer';
-import Budget from '@/Components/Budget/Budget';
+import Budget from '../../Components/Budget/Budget';
 function Dashboard() {
   return (
-    <>
+    <div>
       <Nav />
       <Budget />
       <Footer />
-    </>
+    </div>
   );
 }
 
